b0c6c5c73ce2fbb4434f3c16f5e09ee84c1e903f
Michael Caputo <therubikscubekid@gmail.com>
2015-02-20 22:07:02 -0500

    Added very basic dynamic state taxes and a state select input.

--- controllers/BudgetController.js
+++ controllers/BudgetController.js
@@ -7,8 +7,14 @@ app.controller('BudgetController', function($scope) {
   $scope.totalExpenses = 0;
   $scope.monthlyIncome = 0;
   $scope.savings = 0;
-  $scope.totalExpensesLabel = '0/0'
-  
+  $scope.totalExpensesLabel = '0/0';
+  $scope.state = 'Select State';
+
+  $scope.stateTaxMap = BudgetNamespace.stateTaxMap;
+  $scope.states = ['Select State', 'AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 
+                      'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV',
+                      'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY'];
+  console.log($scope.stateTaxMap);
   
   $scope.initSliders = function() {
     /*
@@ -32,6 +38,8 @@ app.controller('BudgetController', function($scope) {
     $scope.totalExpenses = (parseInt($scope.rent, 10) + parseInt($scope.food, 10) + parseInt($scope.car, 10) + parseInt($scope.savings, 10));
     
     $scope.totalExpensesLabel = $scope.totalExpenses + '/' + $scope.monthlyIncome;
+
+    console.log('State tax: ', $scope.stateTaxMap[$scope.state]);
   };
 
   $scope.update = function() {
@@ -56,7 +64,9 @@ app.controller('BudgetController', function($scope) {
   }
 
   var calculateStateTax = function() {
-    return $scope.income * 0.0515;
+    console.log("Looking up state: ", $scope.state);
+    console.log("Returning tax: ", $scope.stateTaxMap[$scope.state]);
+    return $scope.income * $scope.stateTaxMap[$scope.state];
   }
 
   var calculateSocialSecurityTax = function() {
