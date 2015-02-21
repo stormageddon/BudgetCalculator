20c986eeca71a8a6e07205b62479ec7c87d8bbb7
Michael Caputo <therubikscubekid@gmail.com>
2015-02-20 22:56:33 -0500

    Moved state.js to a constants folder

--- app.js
+++ app.js
@@ -1,53 +1,9 @@
 //Budget Calculator
 var BudgetNamespace = BudgetNamespace || {};
 BudgetNamespace.stateTaxMap = {
-	AR: 0.0090,
-	AZ: 0.0259,
-	CA: 0.0100,
-	CO: 0.0463,
-	CT: 0.0300,
-	DE: 0.0000,
-	FL: 0.0000,
-	GA: 0.0100,
-	HI: 0.0140,
-	IA: 0.0360,
-	ID: 0.0160,
-	IL: 0.0375,
-	IN: 0.0330,
-	KS: 0.0270,
-	KY: 0.0200,
-	LA: 0.0200,
-	MD: 0.0200,
-	ME: 0.0000,
-	MI: 0.0425,
-	MN: 0.0535,
-	MO: 0.0150,
-	MS: 0.0300,
-	MT: 0.0100,
-	NC: 0.0575,
-	ND: 0.0122,
-	NE: 0.0246,
-	NH: 0.0000,
-	NJ: 0.0140,
-	NM: 0.0170,
-	NV: 0.0000,
-	NY: 0.0400,
-	OH: 0.00528,
-	OK: 0.0050,
-	OR: 0.0500,
-	PA: 0.0307,
-	RI: 0.0375,
-	SC: 0.0000,
-	SD: 0.0000,
-	TN: 0.0000,
-	TX: 0.0000,
-	UT: 0.0500,
-	VA: 0.0200,
-	VT: 0.0355,
-	WA: 0.0000,
-	WI: 0.0400,
-	WV: 0.0300,
-	WY: 0.0000
+	
+
+	
 };
 
 var app = angular.module('app', ['ngMaterial', 'ngRoute']);
